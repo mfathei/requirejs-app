938b71dee728a85df2c8a5d75bfe948a4558a8aa
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-04 07:46:20 +0200

    lesson 4

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
 // config.js
+requirejs.config({
+    baseUrl: 'js',
+    paths: {
+        angular: ['https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular.min', 'angular.min'],
+        jquery: 'jquery-3.2.1.min',
+        mootools: 'mootools.min',
+        extCore: 'ext-core.min',
+        swfObject: 'swfobject'
+    }
+});
